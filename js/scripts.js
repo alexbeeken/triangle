8c0b16c4d580bb940b055795be3b4eed1c1cb609
Alex Beeken <aeb242@gmail.com>
2015-02-13 09:41:36 -0800

    find isosceles relationship and pass all specs

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,8 @@ var triangle = function(side1, side2, side3) {
   if ((side2 === side1) && (side2 === side3) && (side3 === side1)) {
     return "equilateral";
   }
+  else if ((side2 === side1) || (side2 === side3) || (side3 === side1)) {
+    return "isosceles";
+  }
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('triangle', function() {
   it("returns equilateral if all sides are equal", function() {
     expect(triangle(1,1,1)).to.equal("equilateral");
   });
+
+  it("returns isosceles if two sides are equal", function() {
+    expect(triangle(1, 1, 2)).to.equal("isosceles");
+  });
 });
