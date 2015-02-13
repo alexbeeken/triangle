b4868683a3b9f605f3411e803d8909bfb6447f25
Alex Beeken <aeb242@gmail.com>
2015-02-13 10:09:31 -0800

    Return not a triangle for inequality problems

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var triangle = function(side1, side2, side3) {
 
-  if ((side1 <= 0) || (side2 <= 0) || (side3 <= 0)) {
+  if (((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side3 + side1 <= side2)) || ((side1 <= 0) || (side2 <= 0) || (side3 <= 0))) {
     return "not a triangle"
   }
   else if ((side2 === side1) && (side2 === side3) && (side3 === side1)) {
--- spec/specs.js
+++ spec/specs.js
@@ -5,14 +5,18 @@ describe('triangle', function() {
   });
 
   it("returns isosceles if two sides are equal", function() {
-    expect(triangle(1, 1, 2)).to.equal("isosceles");
+    expect(triangle(1, 2, 2)).to.equal("isosceles");
   });
 
   it("returns scalene if no sides are equal", function() {
-    expect(triangle(1, 2, 3)).to.equal("scalene");
+    expect(triangle(3, 2, 4)).to.equal("scalene");
   });
 
   it("returns 'not a triangle' if a side is 0", function() {
     expect(triangle(0, 2, 3)).to.equal("not a triangle")
+  });
+
+  it("returns 'not a triangle' if one side is alt least as long as the other two combined", function() {
+    expect(triangle(1, 2, 4)).to.equal("not a triangle")
   });
 });
